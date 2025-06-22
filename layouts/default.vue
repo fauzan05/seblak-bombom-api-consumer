04ffd955b30dd8144e07faffd2bbee3cd40ef283
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2025-06-22 20:21:12 +0700

    Adjust cart badge position in default layout
    Updated the cart badge's CSS classes to move it from 'top-4' to '-top-1', improving its alignment and appearance in the UI.

--- layouts/default.vue
+++ layouts/default.vue
@@ -194,7 +194,7 @@
                                             d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                                     </svg>
                                     <span
-                                        class="absolute top-4 -right-1 bg-red-500 text-white text-[10px] rounded-full h-5 w-5 flex items-center justify-center">
+                                        class="absolute -top-1 -right-1 bg-red-500 text-white text-[10px] rounded-full h-5 w-5 flex items-center justify-center">
                                         3
                                     </span>
                                 </button>
