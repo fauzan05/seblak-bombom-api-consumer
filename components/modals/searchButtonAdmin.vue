eef1c11b129821e234f196095676a38bc8f1a454
Fauzan Nur Hidayat <fauzannurhidayat8@gmail.com>
2025-06-30 09:46:10 +0700

    Remove border from mobile search button
    The border-2 class was removed from the mobile search button in searchButtonAdmin.vue for a cleaner appearance.

--- components/modals/searchButtonAdmin.vue
+++ components/modals/searchButtonAdmin.vue
@@ -2,7 +2,7 @@
     <div>
         <!-- Tombol Search Mobile -->
         <button @click="openSearchModal" 
-                class="md:hidden p-2.5 border-2 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800/50 dark:text-gray-300">
+                class="md:hidden p-2.5 rounded-full hover:bg-gray-100 dark:hover:bg-gray-800/50 dark:text-gray-300">
             <svg xmlns="http://www.w3.org/2000/svg" 
                  class="h-5 w-5 text-gray-600 dark:text-gray-400 group-hover:text-gray-800 dark:group-hover:text-gray-200 transition-colors" 
                  fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2.5" aria-hidden="true">
